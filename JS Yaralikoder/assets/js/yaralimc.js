23d2c8edc68bd2a2c2c6a8e4383a014b5c3613d5
Ramin Atazade <127185803+RaminAtazade@users.noreply.github.com>
2023-04-12 02:43:14 -0700

    Add files via upload

---
+++ JS Yaralikoder/assets/js/yaralimc.js
@@ -0,0 +1,50 @@
+alert ("Bunu yazan tosun, okuyana kuban olsun");
+         
+
+// var a = 21 ;
+// if (a < 10){
+//     alert("tEKREQEMLI");
+// };
+// if (a > 10){
+//     alert("iKIrEQEMLI");
+// }
+
+// var number = prompt("Reqem yaz quzum: ");
+
+// if (number > 9 && number <100){
+//    var tek = number % 10;
+//    var onluq = (number - tek) / 10
+//     if(tek == onluq)
+//     {
+//         alert("Reqemler Beraberdi")
+//     }
+//     else {
+//         alert("Reqemleri ferqlidir")
+//     }
+// }
+// else {
+//     alert("iki reqemli eded yazmalisan")
+// }
+
+
+var a = 1234 ;
+var m = a%10;   
+console.log(m);
+var n = (a%100-m)/10; 
+console.log(n);
+var f = ((a-(m*1+n*10))/100)%10; 
+console.log(f)
+var l = (a-(m*1+n*10+f*100))/1000; 
+console.log(l)
+var c = (m+n+f+l)%2;
+console.log(c)
+
+
+ 
+
+  
+ 
+ 
+ 
+
+
